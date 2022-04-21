e2e92b91f7a364b92828f0bd591349239d303ff1
zobuo <102634998+zobuo@users.noreply.github.com>
2022-04-20 22:11:58 -0700

    lab

---
+++ lab4/js/lab.js
@@ -0,0 +1,14 @@
+// Creating and practicing pseudocode for a game.
+// The n-queens problem finds the amount of different ways queens can be placed on a chessboard
+// so that non of them can attack each other.
+// @link   URL
+// @file   This files defines the pseudocode for n-queens problem.
+// @author Zac Nakamura and Pichy Jumpholwong.
+// @since  4.15.22
+
+// Print the board.
+// Take the user input.
+    // Input any number
+// Validate the user input.
+// Evaluate available spaces on the board.
+// Print all possible solutions.
---
+++ lab5/js/lab.js
@@ -0,0 +1,24 @@
+// Authors: Pichy Jumpholwong and Zac Nakamura
+// Created: 19 April 2022
+// License: Public Domain
+
+// Declare variables make, model, color, year
+var make = "Volkswagen";
+var model = "Beetle";
+var color = "Teal";
+var year = 1960;
+
+// Declare a boolean variable ownIt for whether you own it or not.
+var ownIt = false;
+
+// Calculate age
+var age = 2022 - year;
+
+//Use document.writeln() to neatly output each of your variables, example,
+//Note, that that is WRITELN with an "L" not WRITEIN document.writeln("Make: " + make + "<br>");
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + "<br>");
+document.writeln("Own it? " + ownIt + "<br>");
