c2fa80521fbcd6ca75b8bb3d4bbb017d6919598c
zobuo <102634998+zobuo@users.noreply.github.com>
2022-05-04 23:13:52 -0700

    lab9

---
+++ lab9/js/lab.js
@@ -0,0 +1,16 @@
+// Authors: Pichy Jumpholwong and Zac Nakamura
+// Created: 3 May 2022
+// License: Public Domain
+
+outputEl = document.getElementById("output");
+
+new1El = document.createElement("p");
+// new1El.setAttribute("div", "saucy");
+new1El.innerHTML = "saucy";
+
+var new2El = document.createElement("id");
+// new2El.setAttribute("id", "swaggy");
+new2El.innerHTML = "swaggy";
+
+outputEl.appendChild(new1El);
+// outputEl.appendChild(new2El);
